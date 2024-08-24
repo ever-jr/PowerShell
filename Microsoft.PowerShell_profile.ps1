f8de7d857270b8482dbc0d87d23b60f7d4e3041c
Everton Jr <evertonxdoj@gmail.com>
2024-08-23 21:45:27 -0300

    new amazing profile 😊

--- Microsoft.PowerShell_profile.ps1
+++ Microsoft.PowerShell_profile.ps1
@@ -1,7 +1,3 @@
-#function nvim_fuzzy_find {
-#  nvim $(fzf)
-#}
-
 function Get-GitBranch {
 	$branchName = ""
 	if (Test-Path .git) {
@@ -23,20 +19,6 @@ function Show-Mage {
 	Write-Host "☆ﾟ$branch.*･｡ﾟ" -ForegroundColor Yellow -NoNewLine
 }
 
-function Show-Mage2 {
-    #$user = $env:USERNAME
-    #$path = (Get-Location) -replace "C:", ""
-    #$path = $path -replace "\\", "/"
-    #$path = $path -replace "/Users/$user", "~"
-	$branch = Get-GitBranch
-
-	Write-Host "  / \ " -ForegroundColor Magenta -NoNewLine
-	Write-Host $PWD -ForegroundColor Yellow
-	Write-Host "_/   \__" -ForegroundColor Magenta
-	#Write-Host "(∩｀-´)⊃━☆ﾟ.*･｡ﾟ $branch" -ForegroundColor Blue -NoNewLine
-    Write-Host "(∩｀-´)⊃━☆ﾟ$branch.*･｡ﾟ" -ForegroundColor Blue -NoNewLine
-}
-
 function Prompt {
 	Show-Mage
 	return " "
@@ -45,18 +27,4 @@ function Prompt {
 # disable completion
 Set-PSReadLineOption -PredictionSource None
 
-# Import the Chocolatey Profile that contains the necessary code to enable
-# tab-completions to function for `choco`.
-# Be aware that if you are missing these lines from your profile, tab completion
-# for `choco` will not function.
-# See https://ch0.co/tab-completion for details.
-$ChocolateyProfile = "$env:ChocolateyInstall\helpers\chocolateyProfile.psm1"
-if (Test-Path($ChocolateyProfile)) {
-  Import-Module "$ChocolateyProfile"
-}
-
 Invoke-Expression(&{ (zoxide init powershell | Out-String) })
-
-# ALIASES
-#New-Alias -Name n -Value nvim
-#New-Alias -Name nf -Value nvim_fuzzy_find
---
+++ README.md
@@ -0,0 +1,9 @@
+# Wizard
+
+_PS.: you'll need_ **git** _and_ **zoxide**
+_if you don't want zoxide, delete the following line_
+
+Invoke-Expression(&{ (zoxide init powershell | Out-String) })
+
+
+:)
